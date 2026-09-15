014c84b401bb6befd401f7799e5472b2ed84634d
Tushar k <115905283+tushar7058@users.noreply.github.com>
2026-09-15 23:40:56 +0530

    Solve Domino Piling

--- AlgorithmXlr8-P004-DominoPiling/Solution.py
+++ AlgorithmXlr8-P004-DominoPiling/Solution.py
@@ -1,53 +1,29 @@
 def main():
     n, m = map(int, input().split())
 
-    board = [[False]*m for _ in range(n)]
-    count = 0
+    # 1. Brute force approach 
 
-    for i in range(n):
-        for j in range(m):
-            if board[i][j]:
-                continue
-                # try to place a domino to the right , otherwise try to downword
-            if j+1 < m and not board[i][j+1]:
-                board[i][j] =  board[i][j+1] = True
-                count +=1
-            elif i +1 < n and not board[i+1][j]:
-                board[i][j] = board[i+1][j] = True
-                count +=1
-    print(count)
+    # board = [[False]*m for _ in range(n)]
+    # count = 0
 
-    # Write your solution here.
-    # Find the maximum number of 1x2 dominoes that fit on an n x m board.
+    # for i in range(n):
+    #     for j in range(m):
+    #         if board[i][j]:
+    #             continue
+    #             # try to place a domino to the right , otherwise try to downword
+    #         if j+1 < m and not board[i][j+1]:
+    #             board[i][j] =  board[i][j+1] = True
+    #             count +=1
+    #         elif i +1 < n and not board[i+1][j]:
+    #             board[i][j] = board[i+1][j] = True
+    #             count +=1
+    # print(count)
 
 
-"""
-
-def main():
-    n, m = map(int, input().split())
-    board = [[False] * m for _ in range(n)]
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if board[i][j]:
-                continue
-            # try to place a domino to the right, otherwise try downward
-            if j + 1 < m and not board[i][j + 1]:
-                board[i][j] = board[i][j + 1] = True
-                count += 1
-            elif i + 1 < n and not board[i + 1][j]:
-                board[i][j] = board[i + 1][j] = True
-                count += 1
-    print(count)
+    # 2 . optimized approach
+    print((n*m)//2)
 
 
 if __name__ == "__main__":
     main()
 
-
-
-
-"""
-
-if __name__ == "__main__":
-    main()
