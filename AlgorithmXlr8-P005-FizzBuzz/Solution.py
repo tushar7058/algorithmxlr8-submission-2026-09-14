a0845922aa53e5a67eb1b6dfe97e13349c29f5e7
Tushar k <115905283+tushar7058@users.noreply.github.com>
2026-09-25 02:38:21 +0530

    Solve Fizz Buzz

---
+++ AlgorithmXlr8-P005-FizzBuzz/Solution.py
@@ -0,0 +1,18 @@
+def main():
+    n = int(input())
+    for i in range(1,n+1) :
+        if i %3 == 0 and i %5 ==0:
+            print("FizzBuzz")
+        elif i %3 ==0:
+            print("Fizz")
+        elif i%5 ==0:
+            print("Buzz")
+        else:
+            print(i)
+
+    # Write your solution here.
+    # Print each answer on its own line: "FizzBuzz", "Fizz", "Buzz", or the number.
+
+
+if __name__ == "__main__":
+    main()
